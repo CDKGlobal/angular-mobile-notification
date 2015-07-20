d3ae5053ec34a5fa65b1b94b53a1874ec9929eb0
jayeshgangadharan <jayeshgangadharan@gmail.com>
2015-07-20 11:38:40 -0700

    init

---
+++ pages/home/HomeCtrl.js
@@ -0,0 +1,8 @@
+/**
+*/
+
+'use strict';
+
+angular.module('myApp').controller('HomeCtrl', ['$scope', 'jgMobileNotification', function($scope, jgMobileNotification) {
+	//TODO - put any directive code here
+}]);
